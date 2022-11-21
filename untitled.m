c2ffe0345332a3df043ba13a155a7713d4d208e6
kelyan-hangard <115081956+kelyan-hangard@users.noreply.github.com>
2022-11-21 17:45:46 +0100

    Changé vite fait le main et essayé d'extracter le reste des data

--- untitled.m
+++ untitled.m
@@ -23,6 +23,13 @@ val_st = string(val7);
 tv7_2 = [time7 val_st];
 Time_Value_CM = [Time_Value_CM; tv7_2];
 
+load("raw_data\Substation_Power_02-May15-May-20224555");
+time8 = cell2mat(Power.Subst08CM.time);
+val7 = (Power.Subst08CM.value);
+val_st = string(val7);
+tv7_2 = [time8 val_st];
+Time_Value_CM = [Time_Value_CM; tv7_2];
+
 load("raw_data\Substation_Power_05-Sep18-Sep-2022");
 time7 = cell2mat(Power.Subst08CM.time);
 val7 = (Power.Subst08CM.value);
